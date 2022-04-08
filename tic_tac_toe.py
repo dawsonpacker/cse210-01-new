93e06a552081129b1e279171f4963b2ad52161eb
dawsonpacker <91807575+dawsonpacker@users.noreply.github.com>
2022-04-07 19:16:04 -0600

    k

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -12,37 +12,34 @@ def play_game():
     tic_tac_toe_plays = 1
     while tic_tac_toe_plays < 10:
         print("")
-        print("{}|{}|{}".format(numbers_list[0], numbers_list[1], numbers_list[2]))
+        print(f"{numbers_list[0]}|{numbers_list[1]}|{numbers_list[2]}")
         print("-+-+-")
-        print("{}|{}|{}".format(numbers_list[3], numbers_list[4], numbers_list[5]))
+        print(f"{numbers_list[3]}|{numbers_list[4]}|{numbers_list[5]}")
         print("-+-+-")
-        print("{}|{}|{}".format(numbers_list[6], numbers_list[7], numbers_list[8]))
+        print(f"{numbers_list[6]}|{numbers_list[7]}|{numbers_list[8]}")
         print("")
 
         if x_turn == True: 
-            x = input("x's turn to choose a square (1-9): ")
-            for number in numbers_list: 
-                if x == number: 
-                    index = numbers_list.index(number)
-                    numbers_list[index] = 'x'
+            x = int(input("x's turn to choose a square (1-9): "))
+            if numbers_list[(x - 1)] == x:
+                numbers_list[(x - 1)] = 'x'
+
             x_turn = False
             o_turn = True
         elif o_turn == True: 
-            o = input("o's turn to choose a square (1-9): ")
-            for number in numbers_list: 
-                if o == number: 
-                    index = numbers_list.index(number)
-                    numbers_list[index] = 'o'
+            o = int(input("o's turn to choose a square (1-9): "))
+            if numbers_list[(o - 1)] == o:
+                numbers_list[(o - 1)] = 'o'
             x_turn = True
             o_turn = False
         if (numbers_list[0] == 'x' and numbers_list[1] == 'x' and numbers_list[2] == 'x') or (numbers_list[3] == 'x' and numbers_list[4] == 'x' and numbers_list[5] == 'x') or (numbers_list[6] == 'x' and numbers_list[7] == 'x' and numbers_list[8] == 'x') or (numbers_list[0] == 'x' and numbers_list[3] == 'x' and numbers_list[6] == 'x') or (numbers_list[1] == 'x' and numbers_list[4] == 'x' and numbers_list[7] == 'x') or (numbers_list[4] == 'x' and numbers_list[7] == 'x' and numbers_list[8] == 'x') or (numbers_list[0] == 'x' and numbers_list[4] == 'x' and numbers_list[8] == 'x') or (numbers_list[0] == 'o' and numbers_list[1] == 'o' and numbers_list[2] == 'o') or (numbers_list[3] == 'o' and numbers_list[4] == 'o' and numbers_list[5] == 'o') or (numbers_list[6] == 'o' and numbers_list[7] == 'o' and numbers_list[8] == 'o') or (numbers_list[0] == 'o' and numbers_list[3] == 'o' and numbers_list[6] == 'o') or (numbers_list[1] == 'o' and numbers_list[4] == 'o' and numbers_list[7] == 'o') or (numbers_list[4] == 'o' and numbers_list[7] == 'o' and numbers_list[8] == 'o') or (numbers_list[0] == 'o' and numbers_list[4] == 'o' and numbers_list[8] == 'o'):
             
             print("")
-            print("{}|{}|{}".format(numbers_list[0], numbers_list[1], numbers_list[2]))
+            print(f"{numbers_list[0]}|{numbers_list[1]}|{numbers_list[2]}")
             print("-+-+-")
-            print("{}|{}|{}".format(numbers_list[3], numbers_list[4], numbers_list[5]))
+            print(f"{numbers_list[3]}|{numbers_list[4]}|{numbers_list[5]}")
             print("-+-+-")
-            print("{}|{}|{}".format(numbers_list[6], numbers_list[7], numbers_list[8]))
+            print(f"{numbers_list[6]}|{numbers_list[7]}|{numbers_list[8]}")
             print("")
 
             print('Good Game. We have a winner.')
@@ -51,11 +48,11 @@ def play_game():
 
         if tic_tac_toe_plays == 10: 
             print("")
-            print("{}|{}|{}".format(numbers_list[0], numbers_list[1], numbers_list[2]))
+            print(f"{numbers_list[0]}|{numbers_list[1]}|{numbers_list[2]}")
             print("-+-+-")
-            print("{}|{}|{}".format(numbers_list[3], numbers_list[4], numbers_list[5]))
+            print(f"{numbers_list[3]}|{numbers_list[4]}|{numbers_list[5]}")
             print("-+-+-")
-            print("{}|{}|{}".format(numbers_list[6], numbers_list[7], numbers_list[8]))
+            print(f"{numbers_list[6]}|{numbers_list[7]}|{numbers_list[8]}")
             print("")
 
 if __name__ == "__main__": 
